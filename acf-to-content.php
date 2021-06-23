379ea494f87526cfa7eb6836dd8048908b1230e3
John Huebner <johnhuebner@site-seeker.com>
2021-06-23 12:15:23 -0400

    version

--- acf-to-content.php
+++ acf-to-content.php
@@ -5,7 +5,7 @@
 		Plugin URI: https://github.com/Hube2/acf-to-content
 		GitHub Plugin URI: https://github.com/Hube2/acf-to-content
 		Description: Add ACF fields to post_content for search
-		Version: 1.5.6
+		Version: 1.6.0
 		Author: John A. Huebner II
 		Author URI: https://github.com/Hube2
 		
